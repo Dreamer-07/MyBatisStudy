45bc688c6dd2f0eca08cb93fdcd86e17b3778ab0
EMTKnight <2391105059@qq.com>
2020-09-11 14:27:31 +0800

    MyBatis × HelloWorld √ 实现 MyBatis 实现 CRUD √ 了解 MyBatis 全局配置文件 + SQL 映射文件 √

--- 04_SqlMapper/src/main/java/pers/dreamer07/dao/EmployeeDao.java
+++ 04_SqlMapper/src/main/java/pers/dreamer07/dao/EmployeeDao.java
@@ -1,7 +1,11 @@
 package pers.dreamer07.dao;
 
+import org.apache.ibatis.annotations.MapKey;
 import org.apache.ibatis.annotations.Param;
 import pers.dreamer07.bean.Employee;
+
+import java.util.List;
+import java.util.Map;
 
 //定义操作 Employee 表的规范
 public interface EmployeeDao {
@@ -21,5 +25,16 @@ public interface EmployeeDao {
     //修改Emp对象对应的数据库数据，为修改成功返回false(影响行数为0)，修改成功返回true(影响函数大于0)
     public boolean updateEmp(Employee employee);
 
+    //根据id和empName查询对应的员工信息
     public Employee getEmpByIdAndEmpName(@Param("id") Integer id,@Param("empName") String empName);
+
+    //查询所有信息封装为 List
+    public List<Employee> getEmps();
+
+    //查询一条记录封装的 Map
+    public Map<String,Object> getEmpByIdReturnMap(Integer id);
+
+    //查询多条记录封装的 Map
+    @MapKey("id") //需要通过 @MapKey 注解指定查询结果的字段名作为 返回结果集Map的key值
+    public Map<Integer,Employee> getAllEmpReturnMap();
 }
--- 04_SqlMapper/src/test/java/EmployeeDaoTest.java
+++ 04_SqlMapper/src/test/java/EmployeeDaoTest.java
@@ -9,6 +9,8 @@ import pers.dreamer07.dao.EmployeeDao;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 /*
 * Mybatis的CRUD - 测试类
@@ -43,6 +45,63 @@ public class EmployeeDaoTest {
             //3. 执行方法
             Employee employee = employeeDao.getEmpById(1);
             System.out.println(employee);
+        } finally {
+            //4. 关闭连接
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test
+    public void testSelect2(){
+        SqlSession sqlSession = null;
+        try {
+        /*
+        1. 获取 sqlSession 会话对象
+            - 传入一个布尔值，代表是否开始自动提交事务，默认为false，需要手动提交
+         */
+            sqlSession = sqlSessionFactory.openSession(true);
+            //2. 获取对应的 Dao 实现类
+            EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
+            //3. 执行方法
+            Employee employee = employeeDao.getEmpByIdAndEmpName(1,"emt");
+            System.out.println(employee);
+        } finally {
+            //4. 关闭连接
+            if (sqlSession != null) {
+                sqlSession.close();
+            }
+        }
+    }
+
+    @Test //测试返回集为 List / Map 类型的
+    public void testSelectColl(){
+        SqlSession sqlSession = null;
+        try {
+        /*
+        1. 获取 sqlSession 会话对象
+            - 传入一个布尔值，代表是否开始自动提交事务，默认为false，需要手动提交
+         */
+            sqlSession = sqlSessionFactory.openSession(true);
+            //2. 获取对应的 Dao 实现类
+            EmployeeDao employeeDao = sqlSession.getMapper(EmployeeDao.class);
+            //3. 执行方法
+
+            //3.1 查询由一条 / 多条 记录封装的 List
+//            List<Employee> emps = employeeDao.getEmps();
+//            for (Employee emp : emps) {
+//                System.out.println(emp);
+//            }
+
+            //3.2 查询由一条记录封装的 Map
+//            Map<String, Object> empByIdReturnMap = employeeDao.getEmpByIdReturnMap(1);
+//            System.out.println(empByIdReturnMap); //{gender=1, emp_name=emt, id=1, dept_id=20, email=emt@qq.com}
+
+            //3.3 查询多条记录封装的 Map
+            Map<Integer, Employee> allEmpReturnMap = employeeDao.getAllEmpReturnMap();
+            allEmpReturnMap.keySet().stream().forEach(System.out::println);
+            allEmpReturnMap.values().stream().forEach(System.out::println);
         } finally {
             //4. 关闭连接
             if (sqlSession != null) {
